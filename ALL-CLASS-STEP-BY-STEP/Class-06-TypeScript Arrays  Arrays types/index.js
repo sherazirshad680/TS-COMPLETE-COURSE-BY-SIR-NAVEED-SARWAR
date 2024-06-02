512e8afd26d28a406f075d221b6f59369127642e
Hi EveryOne, I Am Sheraz Irshad! <134085129+sherazirshad680@users.noreply.github.com>
2024-06-02 23:01:35 +0500

    CLASS-06

---
+++ ALL-CLASS-STEP-BY-STEP/Class-06-TypeScript Arrays  Arrays types/index.js
@@ -0,0 +1,15 @@
+"use strict";
+// Array
+// Collections of a variable is called Arrays.
+// with variabls
+var rollNo = 1;
+var rollNo = 2;
+var rollNo = 3;
+var rollNo = 4;
+var rollNo = 5;
+// Same Work withe Arrays but in a arrays means multiple variables in one array
+var studentRollNo = [10, 20, 30, 40, 50, 100, 500];
+studentRollNo[0];
+studentRollNo[2];
+var studentName = ["Sheraz", "Ahmad", "Ali"];
+console.log(studentName);
