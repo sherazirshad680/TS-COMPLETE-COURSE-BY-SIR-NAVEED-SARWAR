635ea65bdaaf1e0fa44e9908d686d81aa4b8265f
Hi EveryOne, I Am Sheraz Irshad! <134085129+sherazirshad680@users.noreply.github.com>
2024-06-03 09:49:13 +0500

    CLASS-13

---
+++ ALL-CLASS-STEP-BY-STEP/Class-13-TypeScript Objects _ Define Objects Types/index.js
@@ -0,0 +1,16 @@
+"use strict";
+// Object
+// How to define the type of Object
+// type CarType = { company: string; isUsedCar: boolean; year: number };
+// var car = {
+//   company: "Honda",
+//   isUsedCar: false,
+//   year: 2014,
+// };
+var car = {
+    company: "Honda",
+    color: "black",
+    isUsedCar: false,
+    year: 2014,
+};
+console.log(car.color);
---
+++ ALL-CLASS-STEP-BY-STEP/Class-13-TypeScript Objects _ Define Objects Types/index.ts
@@ -0,0 +1,17 @@
+// Object
+// How to define the type of Object
+// type CarType = { company: string; isUsedCar: boolean; year: number };
+// var car = {
+//   company: "Honda",
+//   isUsedCar: false,
+//   year: 2014,
+// };
+
+var car = {
+  company: "Honda",
+  color: "black",
+  isUsedCar: false,
+  year: 2014,
+};
+
+console.log(car.color);
