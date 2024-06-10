1e35c39a17cc13dc1b05d28eb7f8e78139ea6fce
Hi EveryOne, I Am Sheraz Irshad! <134085129+sherazirshad680@users.noreply.github.com>
2024-06-11 01:48:46 +0500

    CLASS-16

---
+++ ALL-CLASS-STEP-BY-STEP/Class-16-TypeScript Asynchronous Code_SetTimeout & SetInterval Method/index.js
@@ -0,0 +1,13 @@
+"use strict";
+console.log("Before async code");
+function doSomething() {
+    console.log("print doSomething");
+}
+// with arrow function
+// setTimeout(() => {
+//   console.log("print doSomething");
+// });
+// setTimeout is a async function
+// setTimeout(doSomething, 1000);
+setInterval(doSomething, 1000);
+console.log("After async code");
