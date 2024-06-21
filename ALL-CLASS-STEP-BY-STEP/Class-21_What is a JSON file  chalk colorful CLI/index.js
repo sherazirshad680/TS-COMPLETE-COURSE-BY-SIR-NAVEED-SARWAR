5d02394eb44f6ad632fa8b80cc221dd47ca81e67
Hi EveryOne, I Am Sheraz Irshad! <134085129+sherazirshad680@users.noreply.github.com>
2024-06-21 19:37:40 +0500

    CLASS-21

---
+++ ALL-CLASS-STEP-BY-STEP/Class-21_What is a JSON file  chalk colorful CLI/index.js
@@ -0,0 +1,25 @@
+import chalk from "chalk";
+import countries from "./countries.json" assert { type: "json" };
+// console.log(
+//   chalk.white.bgRed.bold("Hello World!") + "Sheraz" + chalk.bgGreen("Irshad")
+// );
+// for (let index = 0; index < countries.length; index++) {
+//   const element = countries[index];
+//   if (element.name == "Pakistan") {
+//     console.log(
+//       `Country Name is =`,
+//       chalk.white.bgGreen.bold(element.name) +
+//         "Country Code = " +
+//         chalk.white.bgRed.bold(element.code)
+//     );
+//   }
+// }
+for (let index = 0; index < countries.length; index++) {
+    const element = countries[index];
+    if (element.name.slice(0, 1) == "p") {
+        console.log(`Country name is =` +
+            chalk.white.bgRed.bold(element.name) +
+            "Country is =" +
+            chalk.white.bgRed.bold(element.code));
+    }
+}
